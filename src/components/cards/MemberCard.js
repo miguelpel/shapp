d001261958168e5b42a247d9f790b22cfb4ab427
Oleg <oleg.vintonuk@gmail.com>
2018-10-12 15:12:15 +0300

    Rewrited MemberCard component

--- src/components/cards/MemberCard.js
+++ src/components/cards/MemberCard.js
@@ -1,39 +1,39 @@
 import React, { Component } from "react";
-import "./memberCard.css";
-import membersCardData from "../../jsonData/membersCard.json";
+import "./MemberCard.css";
 
-class MemberCard extends Component {
-  state = {
-    dataMembers: membersCardData
-  };
+// JSON link:
+// https://api.myjson.com/bins/13oarw
 
-  render() {
-    const { dataMembers } = this.state;
-    const printMembers = dataMembers.map(member => (
-      <div key={member.id} className="memberCard">
-        <div className="memberCard__left">
-          <img src={`${member.photo}`} alt={`${member.name}`} />
-        </div>
+const MemberCard = ({
+  id,
+  photo,
+  name,
+  surname,
+  telephone,
+  address,
+  organisation,
+  position
+}) => (
+  <div key={id} className="memberCard">
+    <div className="memberCard__left">
+      <img className="memberCadr__image" src={`${photo}`} alt={`${name}`} />
+    </div>
 
-        <div className="organisatinCard__center">
-          <p>{member.surname}</p>
-          <p>{member.name}</p>
-          <p>{member.telephone}</p>
-          <p>{member.address.map(address => `${address}, `)}</p>
-        </div>
+    <div className="memberCard__center">
+      <p className="memberCard__text">{surname}</p>
+      <p className="memberCard__text">{name}</p>
+      <p className="memberCard__text">{telephone}</p>
+      <p className="memberCard__text">{address}</p>
+    </div>
 
-        <div className="memberCard__right">
-          <p>{member.organisation}</p>
-          <p>{member.position}</p>
-          <a className="btn" href="#">
-            Send Mail
-          </a>
-        </div>
-      </div>
-    ));
-
-    return <div>{printMembers}</div>;
-  }
-}
+    <div className="memberCard__right">
+      <p className="memberCard__text">{organisation}</p>
+      <p className="memberCard__text">{position}</p>
+      <a className="btn" href="#">
+        Send Mail
+      </a>
+    </div>
+  </div>
+);
 
 export default MemberCard;
