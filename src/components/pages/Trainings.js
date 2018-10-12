e2709e3b3171e839ccc3c19fed6043945126e1b0
Oleg <oleg.vintonuk@gmail.com>
2018-10-13 00:41:31 +0300

    changed OrganisationsCard and App.css

--- src/components/pages/Trainings.js
+++ src/components/pages/Trainings.js
@@ -1,12 +1,55 @@
-import PageContainer from './PageContainer';
+import PageContainer from "./PageContainer";
+
+import React, { Component } from "react";
+import TrainingCard from "../cards/TrainingCard";
 
 class TrainingsPage extends PageContainer {
-    constructor(props) {
-        super(props);
-        this.state = {
-            pageName: "training"
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      pageName: "training",
+      trainingUrl: "https://api.myjson.com/bins/wjjpw",
+      trainingData: []
+    };
+  }
+
+  componentDidMount() {
+    fetch(this.state.trainingUrl)
+      .then(result => result.json())
+      .then(result => {
+        this.setState({
+          trainingData: result
+        });
+      });
+  }
+
+  render() {
+    const renderTrainings = this.state.trainingData.map(item => {
+      return (
+        <TrainingCard
+          id={item.id}
+          logo={item.logo}
+          nameTraining={item.nameTraining}
+          descriptionTraining={item.descriptionTraining}
+          dateTraining={item.dateTraining}
+          eligibleCountries={item.eligibleCountries.map(item => {
+            return (
+              <img
+                key={`${item}`}
+                className="flag"
+                src={`${item}`}
+                alt="flag logo"
+              />
+            );
+          })}
+          city={item.city}
+          country={item.country}
+        />
+      );
+    });
+
+    return <div>{renderTrainings}</div>;
+  }
 }
 
 export default TrainingsPage;
